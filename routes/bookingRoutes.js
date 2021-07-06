547a7ae7301d8c1c1e5956024f3912be08b5e8ee
Karina <karina.kuss00@gmail.com>
2021-07-06 19:57:10 +0200

    fixes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCheckoutSession } = require('../controllers/bookingController');
+const { getCheckoutSession, getAllBookings, getBooking, createBooking, updateBooking, deleteBooking } = require('../controllers/bookingController');
 const { protect, restrictToRole } = require('../controllers/authController');
 
 const router = express.Router();
@@ -9,9 +9,9 @@ router.use(protect);
 router.get('/checkout-session/:tourId', getCheckoutSession);
 router.get('/', getAllBookings)
 router.post('/', createBooking)
+router.get('/:id', getBooking)
 router.patch('/:id', updateBooking)
 router.delete('/:id', deleteBooking)
-router.get('/:id', getBooking)
 
 router.use(restrictToRole('admin', 'lead-guide'));
 
